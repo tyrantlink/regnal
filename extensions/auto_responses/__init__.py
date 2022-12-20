60eefd950bf499cd6a7705faadbedd55a382799b
TyrantLink <tyrantlink@pm.me>
2022-12-20 11:01:54 -0800

    removed /custom_auto_response in favor of /config

--- extensions/auto_responses/__init__.py
+++ extensions/auto_responses/__init__.py
@@ -3,8 +3,6 @@ from main import client_cls
 
 def setup(client:client_cls) -> None:
 	from .listeners import auto_response_listeners
-	from .commands import auto_response_commands
 
 	client._extloaded()
-	client.add_cog(auto_response_commands(client))
 	client.add_cog(auto_response_listeners(client))
