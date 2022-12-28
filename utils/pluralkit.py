21a6614ba4c3eb73357dcfb54d95b9da165bc878
TyrantLink <tyrantlink@pm.me>
2022-12-28 06:28:42 -0800

    decreased pluralkit delay, wrong file in f757957

--- utils/pluralkit.py
+++ utils/pluralkit.py
@@ -98,7 +98,7 @@ class PluralKit:
 		if req[0]: return Member(req[1])
 		else: return None
 
-	async def get_message(self,message_id:str,delay:float=0.5) -> Message|None:
+	async def get_message(self,message_id:str,delay:float=0.2) -> Message|None:
 		await sleep(delay)
 		req = await self.request(f'/messages/{message_id}')
 		if req[0]: return Message(req[1])
