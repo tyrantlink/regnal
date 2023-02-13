96a609d401990aa6f78441413b9b1919a6366c16
TyrantLink <tyrantlink@pm.me>
2023-02-12 21:37:05 -0800

    am dumbass

--- extensions/statcord.py
+++ extensions/statcord.py
@@ -6,7 +6,7 @@ from client import Client
 
 class statcord_listeners(Cog):
 	def __init__(self,client:Client) -> None:
-		self.api = Statcord(client,self.client.env.statcord_key).start_loop()
+		self.api = Statcord(client,client.env.statcord_key).start_loop()
 	
 	@Cog.listener()
 	async def on_application_command(self,ctx:ApplicationContext) -> None:
