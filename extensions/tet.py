568936ef39f61264e1f0757fe3d0cc368c0eaa9b
TyrantLink <tyrantlink@pm.me>
2023-02-14 21:19:30 -0800

    fixed wrong variable

--- extensions/tet.py
+++ extensions/tet.py
@@ -107,8 +107,7 @@ class tet_stupid_dyno_replacement_bullshit(Cog):
 		role = guild.get_role(role_id)
 		match payload.event_type:
 			case 'REACTION_ADD':
-				await self.client.log.debug('roles list test',roles=[guild.get_role(ROLES.get(r)).name for r in REQUIRES_DISBOARDER])
-				if role_id in REQUIRES_DISBOARDER: await member.remove_roles(*[guild.get_role(ROLES.get(r)) for r in REQUIRES_DISBOARDER])
+				if reaction in REQUIRES_DISBOARDER: await member.remove_roles(*[guild.get_role(ROLES.get(r)) for r in REQUIRES_DISBOARDER])
 				await member.add_roles(role,reason='reaction role add')
 			case 'REACTION_REMOVE':
 				await member.remove_roles(role,reason='reaction role remove')
