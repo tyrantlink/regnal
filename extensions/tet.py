b6daa3f039b252864c09905d4ff30f2fb8977e41
TyrantLink <tyrantlink@pm.me>
2023-02-14 21:38:30 -0800

    hopefully made removing roles much faster

--- extensions/tet.py
+++ extensions/tet.py
@@ -107,7 +107,7 @@ class tet_stupid_dyno_replacement_bullshit(Cog):
 		role = guild.get_role(role_id)
 		match payload.event_type:
 			case 'REACTION_ADD':
-				if reaction in REQUIRES_DISBOARDER: await member.remove_roles(*[guild.get_role(ROLES.get(r)) for r in REQUIRES_DISBOARDER])
+				if reaction in REQUIRES_DISBOARDER: await member.remove_roles(*[guild.get_role(ROLES.get(r)) for r in REQUIRES_DISBOARDER if member.get_role(ROLES.get(r) is not None)],atomic=False,reason='reaction role add')
 				await member.add_roles(role,reason='reaction role add')
 			case 'REACTION_REMOVE':
 				await member.remove_roles(role,reason='reaction role remove')
