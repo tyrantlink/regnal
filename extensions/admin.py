75cabddb17064cace3690521531384ff4a68f69d
TyrantLink <tyrantlink@pm.me>
2023-02-20 02:40:50 -0800

    fixed purge command timeout

--- extensions/admin.py
+++ extensions/admin.py
@@ -25,24 +25,25 @@ class admin_commands(Cog):
 			await ctx.response.send_message(embed=Embed(title='ERROR',description='you must specify either message count or message_id, not both',color=0xff6969),
 				ephemeral=await self.client.hide(ctx))
 			return
+		await ctx.response.defer(ephemeral=await self.client.hide(ctx))
 		if messages:
 			try: purged = await ctx.channel.purge(limit=messages,reason=f'{ctx.author.name} used purge command')
 			except Forbidden:
-				await ctx.response.send_message(embed=Embed(title='PERMISSION ERROR',description='i am missing either `Manage Messages` or `Read Message History`\nbe sure to check both my integration role and channel overrides',color=0xff6969),
+				await ctx.followup.send(embed=Embed(title='PERMISSION ERROR',description='i am missing either `Manage Messages` or `Read Message History`\nbe sure to check both my integration role and channel overrides',color=0xff6969),
 				ephemeral=await self.client.hide(ctx))
 				return
 		if message_id:
 			try: message = Object(message_id)
 			except TypeError:
-				await ctx.response.send_message(embed=Embed(title='ERROR',description='invalid message_id given',color=0xff6969),
+				await ctx.followup.send(embed=Embed(title='ERROR',description='invalid message_id given',color=0xff6969),
 				ephemeral=await self.client.hide(ctx))
 				return
 			try: purged = await ctx.channel.purge(limit=None,after=message.created_at,reason=f'{ctx.author.name} used purge command')
 			except Forbidden:
-				await ctx.response.send_message(embed=Embed(title='PERMISSION ERROR',description='i am missing either `Manage Messages` or `Read Message History`\nbe sure to check both my integration role and channel overrides',color=0xff6969),
+				await ctx.followup.send(embed=Embed(title='PERMISSION ERROR',description='i am missing either `Manage Messages` or `Read Message History`\nbe sure to check both my integration role and channel overrides',color=0xff6969),
 				ephemeral=await self.client.hide(ctx))
 				return
-		await ctx.response.send_message(embed=Embed(title=f'successfully purged {len(purged)} messages',color=await self.client.embed_color(ctx)),ephemeral=await self.client.hide(ctx))
+		await ctx.followup.send(embed=Embed(title=f'successfully purged {len(purged)} messages',color=await self.client.embed_color(ctx)),ephemeral=await self.client.hide(ctx))
 
 def setup(client:Client) -> None:
 	client._extloaded()
