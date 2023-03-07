528d3f17b63402f94d529c6244f8c27bb5b0ec34
TyrantLink <tyrantlink@pm.me>
2023-03-06 17:35:58 -0800

    added log for and ability to remove custom qotd

--- extensions/qotd.py
+++ extensions/qotd.py
@@ -1,10 +1,11 @@
-from discord import Embed,Permissions,Guild,Message,Thread,User
+from discord import Embed,Permissions,Guild,Message,Thread,User,Interaction
+from utils.classes import MakeshiftClass,ApplicationContext,EmptyView
 from discord.commands import Option as option,SlashCommandGroup
-from utils.classes import MakeshiftClass,ApplicationContext
 from datetime import datetime,time as dtime,timedelta
+from discord.errors import Forbidden,NotFound
 from utils.tyrantlib import dev_banned
-from discord.errors import Forbidden
 from discord.ext.commands import Cog
+from discord.ui import Button,button
 from ._shared_vars import questions
 from discord.ext.tasks import loop
 from client import Client
@@ -12,12 +13,38 @@ from random import choice
 from time import time
 
 
+class qotd_new_log_view(EmptyView):
+	def __init__(self,client:Client) -> None:
+		super().__init__(timeout=None)
+		self.client = client
+		self.add_item(self.remove_button)
+
+	@button(
+		label='remove',style=4,
+		custom_id='remove_button')
+	async def remove_button(self,button:Button,interaction:Interaction) -> None:
+		embed_data = interaction.message.embeds[0].fields[0].to_dict()
+		category,question = embed_data.get('name',None),embed_data.get('value',None)
+		match category:
+			case 'added as next question, then discarded':
+				await self.client.db.guild(interaction.guild.id).data.qotd.nextup.remove(question)
+			case 'added as next question, then added to pool':
+				await self.client.db.guild(interaction.guild.id).data.qotd.nextup.remove(question)
+				await self.client.db.guild(interaction.guild.id).data.qotd.pool.remove(question)
+			case 'added to question pool':
+				await self.client.db.guild(interaction.guild.id).data.qotd.pool.remove(question)
+		new_embed = interaction.message.embeds[0]
+		new_embed.description = f'**REMOVED BY {interaction.user.mention}**'
+		await interaction.response.edit_message(embed=new_embed,view=None)
+		await interaction.followup.send('successfully removed question',ephemeral=True)
+
 class qotd_commands(Cog):
 	def __init__(self,client:Client) -> None:
 		self.client = client
 		if 'DEV' in self.client.flags.keys():
 			self.qotd_loop.change_interval(time=(datetime.now()+timedelta(seconds=20)).astimezone(datetime.now().astimezone().tzinfo).timetz())
 		self.qotd_loop.start()
+		self.client.add_view(qotd_new_log_view(self.client))
 
 	def cog_unload(self) -> None:
 		self.qotd_loop.cancel()
@@ -105,18 +132,27 @@ class qotd_commands(Cog):
 	@dev_banned()
 	async def slash_qotd_add_question(self,ctx:ApplicationContext,type:str,question:str) -> None:
 		embed = Embed(title='successfully added a qotd question',color=await self.client.embed_color(ctx))
+		log_embed = Embed(color=await self.client.embed_color(ctx))
 		match type:
 			case 'add as next question':
 				await self.client.db.guild(ctx.guild.id).data.qotd.nextup.append(question)
 				embed.add_field(name='added as next question, then discarded',value=question)
+				log_embed.add_field(name='added as next question, then discarded',value=question)
 			case 'add as next question, then add to pool':
 				await self.client.db.guild(ctx.guild.id).data.qotd.nextup.append(question)
 				await self.client.db.guild(ctx.guild.id).data.qotd.pool.append(question)
 				embed.add_field(name='added as next question, then added to pool',value=question)
+				log_embed.add_field(name='added as next question, then added to pool',value=question)
 			case 'add to question pool':
 				await self.client.db.guild(ctx.guild.id).data.qotd.pool.append(question)
 				embed.add_field(name='added to question pool',value=question)
+				log_embed.add_field(name='added to question pool',value=question)
 		await ctx.response.send_message(embed=embed,ephemeral=await self.client.hide(ctx))
+		if log_channel:=await self.client.db.guild(ctx.guild.id).config.logging.channel.read():
+			try: channel = ctx.guild.get_channel(log_channel) or await ctx.guild.fetch_channel(log_channel)
+			except (NotFound,Forbidden): return
+			log_embed.set_author(name=f'{ctx.author.display_name} used /qotd add_question',icon_url=ctx.author.display_avatar.url)
+			await channel.send(embed=log_embed,view=qotd_new_log_view(self.client))
 
 	@loop(time=dtime(9,0,tzinfo=datetime.now().astimezone().tzinfo))
 	async def qotd_loop(self) -> None:
