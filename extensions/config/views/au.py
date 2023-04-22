8e05db9e1966b7558ad19b9bffa9d724eabba94b
TyrantLink <tyrantlink@pm.me>
2023-04-22 04:00:11 -0700

    removed debug code

--- extensions/config/views/au.py
+++ extensions/config/views/au.py
@@ -265,9 +265,6 @@ class au_view(EmptyView):
 		label='save',style=3,row=4,
 		custom_id='save_button',disabled=True)
 	async def save_button(self,button:Button,interaction:Interaction) -> None:
-		print(self.selected_au.trigger)
-		print(self.past_au.trigger)
-		return
 		if self.selected_au.trigger != self.past_au.trigger: self.au.pop(self.past_au.trigger)
 		self.au[self.selected_au.trigger] = self.selected_au.to_dict()
 		if self.base: await self.client.db.inf('/reg/nal').auto_responses.write(self.selected_au.to_dict(False),[self.selected_au.trigger])
