e2b2a67def47b8d757d1c792b037fcc102fe8821
tyrantlink <tyrantlink@pm.me>
2024-04-16 08:15:09 -0700

    tts banning now logged to logging channel

--- extensions/tts/views/banning.py
+++ extensions/tts/views/banning.py
@@ -2,6 +2,7 @@ from discord import Member,Embed,Interaction,ButtonStyle
 from discord.ui import button,user_select,Button,Select
 from utils.pycord_classes import SubView,MasterView
 from utils.db.documents import Guild as GuildDoc
+from typing import Literal
 
 
 class TTSBanningView(SubView):
@@ -34,6 +35,29 @@ class TTSBanningView(SubView):
 				else f'{banned_users[:1021]}...')
 				or 'None')
 
+	async def log_action(self,action:Literal['ban','unban'],old_value:set[int]) -> None:
+		guild_doc = await self.client.db.guild(self.user.guild.id)
+		if (
+			not guild_doc.config.logging.enabled or
+			guild_doc.config.logging.channel is None or
+			not guild_doc.config.logging.log_commands
+		): return
+
+		channel = self.user.guild.get_channel(guild_doc.config.logging.channel) or await self.client.fetch_channel(guild_doc.config.logging.channel)
+		if channel is None: return
+
+		changed = self.selected_users - old_value if action == 'ban' else self.selected_users & old_value
+
+		embed = Embed(
+			color = 0xff6969 if action == 'ban' else 0xffff69)
+		embed.set_author(
+			name = f'{self.user.display_name} {action}ned users from tts',
+			icon_url = self.user.avatar.url)
+		embed.add_field(
+			name = f'{action}ned users',
+			value = '\n'.join([f'<@{i}>' for i in changed]))
+		await channel.send(embed=embed)
+
 	@user_select(
 		placeholder = 'select a users to ban',
 		row = 0,
@@ -50,6 +74,7 @@ class TTSBanningView(SubView):
 		custom_id = 'button_ban')
 	async def button_ban(self,button:Button,interaction:Interaction) -> None:
 		guild_doc = await self.client.db.guild(self.user.guild.id)
+		await self.log_action('ban',set(guild_doc.data.tts.banned))
 		guild_doc.data.tts.banned = list(set(guild_doc.data.tts.banned) | self.selected_users)
 		await guild_doc.save_changes()
 		await self.reload_embed(guild_doc)
@@ -62,6 +87,7 @@ class TTSBanningView(SubView):
 		custom_id = 'button_unban')
 	async def button_unban(self,button:Button,interaction:Interaction) -> None:
 		guild_doc = await self.client.db.guild(self.user.guild.id)
+		await self.log_action('unban',set(guild_doc.data.tts.banned))
 		guild_doc.data.tts.banned = list(set(guild_doc.data.tts.banned) - self.selected_users)
 		await guild_doc.save_changes()
 		await self.reload_embed(guild_doc)
