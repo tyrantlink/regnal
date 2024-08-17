03b8e421fbfcd80686f702493306308597b54d18
tyrantlink <tyrantlink@pm.me>
2024-08-17 10:32:42 -0700

    added secret loading log

--- extensions/auto_responses/__init__.py
+++ extensions/auto_responses/__init__.py
@@ -1,6 +1,7 @@
 from .listeners import ExtensionAutoResponsesListeners
 from .commands import ExtensionAutoResponsesCommands
 from .logic import ExtensionAutoResponsesLogic
+from au_scripts.aulib import SECRETS
 from .config import register_config
 from .classes import AutoResponses
 from client import Client
@@ -26,3 +27,11 @@ def setup(client: Client) -> None:
     register_config(client.config)
 
     client.add_cog(ExtensionAutoResponses(client))
+
+    loaded_secrets = ", ".join(
+        k for k, v
+        in SECRETS.__dict__.items()
+        if v is not None
+    )
+
+    client.log.info(f'loaded au_script secrets: {loaded_secrets}')
