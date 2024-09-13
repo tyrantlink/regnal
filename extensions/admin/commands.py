7a1482f4175dd762a34bb9007242b0d095249f4b
tyrantlink <38902185+tyrantlink@users.noreply.github.com>
2024-09-13 07:02:24 -0700

    added timeout command

--- extensions/admin/commands.py
+++ extensions/admin/commands.py
@@ -1,5 +1,7 @@
-from discord import slash_command, Permissions, Option, ApplicationContext, Embed, message_command, Message, InteractionContextType
+from discord import slash_command, Permissions, Option, ApplicationContext, Embed, message_command, Message, InteractionContextType, Member
 from .subcog import ExtensionAdminSubCog
+from datetime import timedelta
+from time import time
 
 
 class ExtensionAdminCommands(ExtensionAdminSubCog):
@@ -17,7 +19,65 @@ class ExtensionAdminCommands(ExtensionAdminSubCog):
         )
 
         await ctx.response.send_message(embed=Embed(
-            title=f'successfully purged {len(purged)} message{"" if len(purged) == 1 else "s"}',
+            title=f'successfully purged {len(purged)} message{
+                "" if len(purged) == 1 else "s"}',
+            color=await self.client.helpers.embed_color(ctx.guild_id)),
+            ephemeral=await self.client.helpers.ephemeral(ctx)
+        )
+
+    @slash_command(
+        name='timeout',
+        description='custom timeout down to the second (up to 28 days)',
+        default_member_permissions=Permissions(moderate_members=True),
+        contexts={InteractionContextType.guild},
+        options=[
+            Option(Member, name='member',
+                   description='member to timeout', required=True),
+            Option(int, name='days', description='message to purge until',
+                   default=0, max_value=28),
+            Option(int, name='hours', description='message to purge until',
+                   default=0, max_value=28*24),
+            Option(int, name='minutes', description='message to purge until',
+                   default=0, max_value=28*24*60),
+            Option(int, name='seconds', description='message to purge until',
+                   default=0, max_value=28*24*60*60)])
+    async def slash_timeout(
+        self,
+        ctx: ApplicationContext,
+        member: Member,
+        days: int,
+        hours: int,
+        minutes: int,
+        seconds: int
+    ) -> None:
+        timeout = timedelta(
+            days=days,
+            hours=hours,
+            minutes=minutes,
+            seconds=seconds
+        )
+
+        if timeout.total_seconds() == 0:
+            await ctx.response.send_message(embed=Embed(
+                title='timeout cannot be zero',
+                color=await self.client.helpers.embed_color(ctx.guild_id)),
+                ephemeral=await self.client.helpers.ephemeral(ctx)
+            )
+            return
+
+        if timeout.total_seconds() > 28*24*60*60:
+            await ctx.response.send_message(embed=Embed(
+                title='timeout cannot exceed 28 days (2419200 seconds)',
+                color=await self.client.helpers.embed_color(ctx.guild_id)),
+                ephemeral=await self.client.helpers.ephemeral(ctx)
+            )
+            return
+
+        await member.timeout_for(timeout, reason=f'{ctx.author.name} used /timeout')
+
+        await ctx.response.send_message(embed=Embed(
+            title=f'successfully timed out {member.mention} for until <t:{
+                int(time()+timeout.total_seconds())}:f>',
             color=await self.client.helpers.embed_color(ctx.guild_id)),
             ephemeral=await self.client.helpers.ephemeral(ctx)
         )
@@ -35,7 +95,8 @@ class ExtensionAdminCommands(ExtensionAdminSubCog):
         )
 
         await ctx.response.send_message(embed=Embed(
-            title=f'successfully purged {len(purged)} message{"" if len(purged) == 1 else "s"}',
+            title=f'successfully purged {len(purged)} message{
+                "" if len(purged) == 1 else "s"}',
             color=await self.client.helpers.embed_color(ctx.guild_id)),
             ephemeral=await self.client.helpers.ephemeral(ctx)
         )
